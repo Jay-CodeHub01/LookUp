52f7b3055dc3df226bec511792c4f79a76a3a71b
Jay-CodeHube01 <pateljay18v@email.com>
2026-02-27 17:33:53 +0530

    fix cloudinary: image upload problem

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ connectDB();
 const app = express();
 
 // -------- MIDDLEWARE --------
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
